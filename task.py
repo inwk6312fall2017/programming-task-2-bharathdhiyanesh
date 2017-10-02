ff5bb1d9ba951507f775189528745073060870ef
bhararthdhiyanesh <Bharath.selvaraj@dal.ca>
2017-10-02 15:55:45 +0000

    testing1

---
+++ task.py
@@ -0,0 +1,17 @@
+import os
+with open(Crime.csv,r) as file
+	data=file.read()
+print(data)
+def crime():
+	crime_type=[]
+	crime_id=[]
+	crime_count=[]
+	
+		for item in data:
+		a=crime_type.append(data)
+		b=crime_id.append(data)
+		c=crime_count.append(data)
+		d=a.split()
+		f=b.split()
+		
+	
